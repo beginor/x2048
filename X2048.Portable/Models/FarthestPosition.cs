9e6340f61bd9d15e2f3665d4762c5cc04098134c
beginor <beginor@qq.com>
2014-09-15 20:15:41 +0800

    init check in.

---
+++ X2048.Portable/Models/FarthestPosition.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.Linq;
+
+namespace Beginor.X2048.Models {
+
+    public class FarthestPosition {
+
+        public Cell Farthest { get; set; }
+        public Cell Next { get; set; }
+
+    }
+}
