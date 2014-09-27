c38f5bbbdc16bc8f5007b096ac7887a4d2beae54
beginor <beginor@qq.com>
2014-09-27 11:36:04 +0800

    move renderer export

--- X2048.Touch/Properties/AssemblyInfo.cs
+++ X2048.Touch/Properties/AssemblyInfo.cs
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: Xamarin.Forms.ExportRenderer(typeof(Beginor.X2048.Views.GridView), typeof(Beginor.X2048.GameViewRenderer))]
